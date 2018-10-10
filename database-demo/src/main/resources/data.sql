b418d0c762b4aed6dc56755f0cbc90e1ae03dbf9
Joao Sa <joao.sa@alert-online.com>
2018-10-10 12:15:02 +0100

    Database demo - initial commit

---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+create table person
+(
+	id integer not null,
+	name varchar(255) not null,
+	location varchar(255),
+	birth_date timestamp,
+	primary key(id)
+);
+
+insert into person (id, name, location, birth_date) values (10001, 'Joao', 'Gondomar', sysdate());
+insert into person (id, name, location, birth_date) values (10002, 'Dri', 'Porto', sysdate());
+insert into person (id, name, location, birth_date) values (10003, 'Manel', 'Braga', sysdate());
