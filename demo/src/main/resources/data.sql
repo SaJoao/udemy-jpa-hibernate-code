46206a0a2cc2897f0d6bcd284dcc5b2944c90e4e
Joao Sa <sa.joaopedro@gmail.com>
2018-12-16 22:54:09 +0000

    Jpa and Hiberbate in depth - Step 103

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -3,7 +3,7 @@ insert into course (id, name, created_date, last_updated_date) values (10002, 'S
 insert into course (id, name, created_date, last_updated_date) values (10003, 'Spring Boot in 100 steps', sysdate(), sysdate());
 
 insert into passport (id, number) values (40001, 'PT0191919');
-insert into passport (id, number) values (40002, 'PT3534565');
+insert into passport (id, number) values (40002, 'PT123465');
 insert into passport (id, number) values (40003, 'PT2345435');
 
 insert into student (id, name, passport_id) values (20001, 'Joao', 40001);
--- demo/src/test/java/com/in28minutes/jpa/hibernate/demo/JPQLTest.java
+++ demo/src/test/java/com/in28minutes/jpa/hibernate/demo/JPQLTest.java
@@ -3,6 +3,7 @@ package com.in28minutes.jpa.hibernate.demo;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.junit.Test;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Student;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,8 +29,8 @@ public class JPQLTest {
 	@Test
 	public void getAllCourses() {
 
-		TypedQuery<Course> namedQuery = em.createNamedQuery("query_get_all_courses", Course.class);
-		List<Course> resultList = namedQuery.getResultList();
+		TypedQuery<Course> query = em.createNamedQuery("query_get_all_courses", Course.class);
+		List<Course> resultList = query.getResultList();
 
 		logger.info("Courses list: {}", resultList);
 	}
@@ -36,10 +38,76 @@ public class JPQLTest {
 	@Test
 	public void get100StepsCourses() {
 
-		TypedQuery<Course> namedQuery = em.createNamedQuery("query_get_100_steps_courses", Course.class);
-		List<Course> resultList = namedQuery.getResultList();
+		TypedQuery<Course> query = em.createNamedQuery("query_get_100_steps_courses", Course.class);
+		List<Course> resultList = query.getResultList();
 
 		logger.info("Courses list: {}", resultList);
 	}
 
+	@Test
+	public void jpql_courses_without_students() {
+
+		TypedQuery<Course> query = em.createQuery("select c from Course c where c.students is empty", Course.class);
+		List<Course> resultList = query.getResultList();
+
+		logger.info("Courses without students: {}", resultList);
+	}
+
+	@Test
+	public void jpql_courses_ordered_by_number_of_students() {
+
+		TypedQuery<Course> query = em.createQuery("select c from Course c order by size(c.students)", Course.class);
+		List<Course> resultList = query.getResultList();
+
+		logger.info("Courses ordered by number of students: {}", resultList);
+	}
+
+	@Test
+	public void jpql_students_with_passports_in_a_certain_pattern() {
+
+		TypedQuery<Student> query = em.createQuery("select s from Student s where s.passport.number like '%1234%'",
+				Student.class);
+		List<Student> resultList = query.getResultList();
+
+		logger.info("Student having passport numbers that include 1234: {}", resultList);
+	}
+
+	@Test
+	public void join() {
+
+		Query query = em.createQuery("select c, s from Course c JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+
+		logger.info("Number of results (inner) : {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course: {} Student: {}", result[0], result[1]);
+
+		}
+	}
+
+	@Test
+	public void left_join() {
+
+		Query query = em.createQuery("select c, s from Course c LEFT JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+
+		logger.info("Number of results (left) : {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course: {} Student: {}", result[0], result[1]);
+
+		}
+	}
+
+	@Test
+	public void cross_join() {
+
+		Query query = em.createQuery("select c, s from Course c, Student s");
+		List<Object[]> resultList = query.getResultList();
+
+		logger.info("Number of results (cross) : {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course: {} Student: {}", result[0], result[1]);
+
+		}
+	}
 }
